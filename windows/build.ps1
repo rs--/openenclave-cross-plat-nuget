45937b673e94cb3319e893ca7d2af360569613bc
Hernan Gatta <hegatta@microsoft.com>
2020-09-11 06:50:33 +0000

    Build scripts for Cross-Platform Open Enclave NuGet package
    Contains scripts for building and packaging the Open Enclave SDK v0.8.1
    for:
    
    x64:
    ====
    * Windows / SGX / Legacy / Debug
    * Windows / SGX / Legacy / Release
    * Windows / SGX / FLC / Debug
    * Windows / SGX / FLC / Release
    
    * Linux / SGX / Legacy / Debug
    * Linux / SGX / Legacy / Release
    * Linux / SGX / FLC / Debug
    * Linux / SGX / FLC / Release
    
    AArch64:
    ========
    * Linux / OP-TEE 3.6.0 / QEMU ARMv8 / Debug
    * Linux / OP-TEE 3.6.0 / LS-1012A / Debug
    * Linux / OP-TEE 3.6.0 / QEMU ARMv8 / Release
    * Linux / OP-TEE 3.6.0 / LS-1012A / Release
    
    Signed-off-by: Hernan Gatta <hegatta@microsoft.com>

---
+++ windows/build.ps1
@@ -0,0 +1,54 @@
+﻿# Build for SGX
+# |- Legacy
+#    |- Debug
+#    |- Release
+# |- FLC
+#    |- Debug
+#    |- Release
+
+$ErrorActionPreference = "Stop"
+
+If (-not (Test-Path -Path SDK))
+{
+    git clone --recursive --depth=1 https://github.com/openenclave/openenclave SDK
+}
+
+$SDK_PATH = (Join-Path -Path $PWD -ChildPath SDK)
+
+If (Test-Path Build)
+{
+    Remove-Item Build -Recurse -Force
+}
+
+New-Item -ItemType Directory -Path Build\Legacy\Debug | Out-Null
+New-Item -ItemType Directory -Path Build\Legacy\Release | Out-Null
+New-Item -ItemType Directory -Path Build\FLC\Debug | Out-Null
+New-Item -ItemType Directory -Path Build\FLC\Release | Out-Null
+
+Push-Location -Path Build\Legacy\Debug
+cmake $SDK_PATH -G Ninja -DNUGET_PACKAGE_PATH=C:\oe_prereqs -DBUILD_ENCLAVES=ON -DCPACK_GENERATOR=NuGet -DHAS_QUOTE_PROVIDER=OFF -DCMAKE_BUILD_TYPE=Debug
+ninja
+cpack.exe -D CPACK_NUGET_COMPONENT_INSTALL=ON -DCPACK_COMPONENTS_ALL=OEHOSTVERIFY
+cpack.exe
+Pop-Location
+
+Push-Location -Path Build\Legacy\Release
+cmake $SDK_PATH -G Ninja -DNUGET_PACKAGE_PATH=C:\oe_prereqs -DBUILD_ENCLAVES=ON -DCPACK_GENERATOR=NuGet -DHAS_QUOTE_PROVIDER=OFF -DCMAKE_BUILD_TYPE=Release
+ninja
+cpack.exe -D CPACK_NUGET_COMPONENT_INSTALL=ON -DCPACK_COMPONENTS_ALL=OEHOSTVERIFY
+cpack.exe
+Pop-Location
+
+Push-Location -Path Build\FLC\Debug
+cmake $SDK_PATH -G Ninja -DNUGET_PACKAGE_PATH=C:\oe_prereqs -DBUILD_ENCLAVES=ON -DCPACK_GENERATOR=NuGet -DHAS_QUOTE_PROVIDER=ON -DCMAKE_BUILD_TYPE=Debug
+ninja
+cpack.exe -D CPACK_NUGET_COMPONENT_INSTALL=ON -DCPACK_COMPONENTS_ALL=OEHOSTVERIFY
+cpack.exe
+Pop-Location
+
+Push-Location -Path Build\FLC\Release
+cmake $SDK_PATH -G Ninja -DNUGET_PACKAGE_PATH=C:\oe_prereqs -DBUILD_ENCLAVES=ON -DCPACK_GENERATOR=NuGet -DHAS_QUOTE_PROVIDER=ON -DCMAKE_BUILD_TYPE=Release
+ninja
+cpack.exe -D CPACK_NUGET_COMPONENT_INSTALL=ON -DCPACK_COMPONENTS_ALL=OEHOSTVERIFY
+cpack.exe
+Pop-Location
---
+++ windows/pack.ps1
@@ -0,0 +1,90 @@
+﻿$ErrorActionPreference = "Stop"
+
+$LibCopyRules = @(
+    New-Object PSObject -Property @{Source="$PWD\Build\Legacy\Debug";Destination="$PWD\Pack\lib\native\win\sgx\legacy\debug"}
+    New-Object PSObject -Property @{Source="$PWD\Build\Legacy\Release";Destination="$PWD\Pack\lib\native\win\sgx\legacy\release"}
+
+    New-Object PSObject -Property @{Source="$PWD\Build\FLC\Debug";Destination="$PWD\Pack\lib\native\win\sgx\flc\debug"}
+    New-Object PSObject -Property @{Source="$PWD\Build\FLC\Release";Destination="$PWD\Pack\lib\native\win\sgx\flc\release"}
+)
+
+If (Test-Path Pack)
+{
+    Remove-Item Pack -Recurse -Force
+}
+
+Function Get-LibsByGlob([String]$Glob)
+{
+    Get-ChildItem -Recurse $Glob |
+        Where-Object { -Not ($_.FullName.Contains("tests") -or $_.FullName.Contains("tools") -or $_.FullName.Contains("debugger")) }
+}
+
+Function Get-EnclaveLibs()
+{
+    Get-LibsByGlob *.a
+}
+
+Function Get-HostLibs()
+{
+    Get-LibsByGlob *.lib
+}
+
+Function Copy-Tools([String]$SgxPlatform)
+{
+    Push-Location $PWD\Build\$SgxPlatform\Release\_CPack_Packages\win64\NuGet
+    $Bin = (Get-ChildItem -Recurse bin)
+    Pop-Location
+    Copy-Item -Path "$($Bin.FullName)\*" -Destination .\Pack\tools\win\legacy -Recurse -Force
+}
+
+Function Copy-Includes([String]$SgxPlatform, [String]$BuildType)
+{
+    Push-Location $PWD\Build\$SgxPlatform\$BuildType\_CPack_Packages\win64\NuGet
+    $Inc = (Get-ChildItem -Recurse include)
+    Pop-Location
+
+    Copy-Item -Path $Inc.FullName -Destination .\Pack\build\native\win\sgx\$SgxPlatform\$BuildType\ -Recurse -Force
+}
+
+
+New-Item -ItemType Directory -Path Pack\build\native\win\sgx\legacy\debug | Out-Null
+New-Item -ItemType Directory -Path Pack\build\native\win\sgx\legacy\release | Out-Null
+
+New-Item -ItemType Directory -Path Pack\build\native\win\sgx\flc\debug | Out-Null
+New-Item -ItemType Directory -Path Pack\build\native\win\sgx\flc\release | Out-Null
+
+
+New-Item -ItemType Directory -Path Pack\lib\native\win\sgx\legacy\debug\enclave\clang-7 | Out-Null
+New-Item -ItemType Directory -Path Pack\lib\native\win\sgx\legacy\debug\host\msvc-14.16.27023 | Out-Null
+
+New-Item -ItemType Directory -Path Pack\lib\native\win\sgx\legacy\release\enclave\clang-7 | Out-Null
+New-Item -ItemType Directory -Path Pack\lib\native\win\sgx\legacy\release\host\msvc-14.16.27023 | Out-Null
+
+
+New-Item -ItemType Directory -Path Pack\lib\native\win\sgx\flc\debug\enclave\clang-7 | Out-Null
+New-Item -ItemType Directory -Path Pack\lib\native\win\sgx\flc\debug\host\msvc-14.16.27023 | Out-Null
+
+New-Item -ItemType Directory -Path Pack\lib\native\win\sgx\flc\release\enclave\clang-7 | Out-Null
+New-Item -ItemType Directory -Path Pack\lib\native\win\sgx\flc\release\host\msvc-14.16.27023 | Out-Null
+
+
+New-Item -ItemType Directory -Path Pack\tools\win\legacy | Out-Null
+New-Item -ItemType Directory -Path Pack\tools\win\flc | Out-Null
+
+
+$LibCopyRules | ForEach-Object {
+    Push-Location $_.Source
+    $EnclaveLibs = Get-EnclaveLibs
+    $HostLibs = Get-HostLibs
+    Copy-Item -Path $EnclaveLibs -Destination (Join-Path -Path $_.Destination -ChildPath enclave\clang-7)
+    Copy-Item -Path $HostLibs -Destination (Join-Path -Path $_.Destination -ChildPath host\msvc-14.16.27023)
+    Pop-Location
+}
+
+Copy-Includes Legacy Debug
+Copy-Includes Legacy Release
+Copy-Includes FLC Debug
+Copy-Includes FLC Release
+
+Copy-Tools Legacy
+Copy-Tools FLC
